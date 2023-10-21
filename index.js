0624b4ce637f4e02f65cb80325430a04140ea457
Akari <settd2002@gmail.com>
2023-10-21 22:35:30 +0200

    Added Number Formatting

--- index.js
+++ index.js
@@ -9,18 +9,22 @@ let player = {
 
 document.getElementById('crazyButton').addEventListener('click', function(event){
     player.crazy++;
-    document.getElementById('crazyDisplay').innerText=player.crazy;
+    document.getElementById('crazyDisplay').innerText=formatNumber(player.crazy);
 });
 
 document.getElementById('generatorButton').addEventListener('click', function(event){
     player.generatorCount++;
-    document.getElementById('generatorDisplay').innerText=player.generatorCount;
+    document.getElementById('generatorDisplay').innerText=formatNumber(player.generatorCount);
 });
+
+function formatNumber(number) {
+    return number.toFixed(0);
+}
 
 function tick(deltaTime) {
     const multiplier = deltaTime/1000;
     player.crazy += player.generatorCount * multiplier;
-    document.getElementById('crazyDisplay').innerText=player.crazy;
+    document.getElementById('crazyDisplay').innerText=formatNumber(player.crazy);
 }
 
 let lastTime = performance.now();
