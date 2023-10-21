821c8a77dda1b2669548257a794be7fa1ba9bed8
Akari <settd2002@gmail.com>
2023-10-21 22:24:17 +0200

    Added game loop

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ let player = {
 }
 
 
+
 document.getElementById('crazyButton').addEventListener('click', function(event){
     player.crazy++;
     document.getElementById('crazyDisplay').innerText=player.crazy;
@@ -15,3 +16,19 @@ document.getElementById('generatorButton').addEventListener('click', function(ev
     player.generatorCount++;
     document.getElementById('generatorDisplay').innerText=player.generatorCount;
 });
+
+function tick(deltaTime) {
+    const multiplier = deltaTime/1000;
+    player.crazy += player.generatorCount * multiplier;
+    document.getElementById('crazyDisplay').innerText=player.crazy;
+}
+
+let lastTime = performance.now();
+function loop() {
+    const currentTime = performance.now();
+    const deltaTime = currentTime - lastTime;
+    lastTime = currentTime;
+    tick(deltaTime);
+}
+
+setInterval(loop,1000);
